f72cef8913f2b04ed47f2a8e15f5d79c485ab2fb
Yutaka Sano <sano_yutaka_gn@cyberagent.co.jp>
2016-03-08 20:09:54 +0900

    clean indent

--- src/AtUtil.ts
+++ src/AtUtil.ts
@@ -5,30 +5,30 @@
 
 class ATUtil {
 
-		constructor(){
-		}
+    constructor() {
+    }
 
-		public randomRange(min, max) {
-				return min + Math.random() * (max - min);
-		}
+    public randomRange(min, max) {
+        return min + Math.random() * (max - min);
+    }
 
-		public randomInt(min, max) {
-				return Math.floor(min + Math.random() * (max - min + 1));
-		}
-
-
-		public lerp(value, min, max) {
-				return min + (max - min) * value;
-		}
+    public randomInt(min, max) {
+        return Math.floor(min + Math.random() * (max - min + 1));
+    }
 
 
-		public norm(value, min, max) {
-				return (value - min) / (max - min);
-		}
+    public lerp(value, min, max) {
+        return min + (max - min) * value;
+    }
 
-		public map(value, min1, max1, min2, max2) {
-				return this.lerp(this.norm(value, min1, max1), min2, max2);
-		}
+
+    public norm(value, min, max) {
+        return (value - min) / (max - min);
+    }
+
+    public map(value, min1, max1, min2, max2) {
+        return this.lerp(this.norm(value, min1, max1), min2, max2);
+    }
 
 //compile error
 //		public shuffle(o:string) {
@@ -36,7 +36,7 @@ class ATUtil {
 //				return o;
 //		}
 
-		public clamp(value, min, max) {
-				return Math.max(Math.min(value, max), min);
-		}
+    public clamp(value, min, max) {
+        return Math.max(Math.min(value, max), min);
+    }
 }
--- src/Visualizer.ts
+++ src/Visualizer.ts
@@ -2,25 +2,25 @@
 /// <reference path="AudioManager.ts" />
 
 class Visualizer {
-		private viewManager:ViewManager;
-		private audioManager:AudioManager;
+    private viewManager:ViewManager;
+    private audioManager:AudioManager;
 
-		constructor() {
-				this.audioManager = new AudioManager();
-				this.audioManager.initialize();
-				this.audioManager.loadSampleAudio();
+    constructor() {
+        this.audioManager = new AudioManager();
+        this.audioManager.initialize();
+        this.audioManager.loadSampleAudio();
 
-				this.viewManager = new ViewManager();
-				this.viewManager.initialize();
+        this.viewManager = new ViewManager();
+        this.viewManager.initialize();
 
-		}
+    }
 
-		public start() {
-			this.viewManager.animate();
-		}
+    public start() {
+        this.viewManager.animate();
+    }
 }
 
 window.addEventListener("load", (e) => {
-		var main:Visualizer = new Visualizer();
-		main.start();
+    var main:Visualizer = new Visualizer();
+    main.start();
 });
